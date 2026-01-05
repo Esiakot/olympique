32755812e44615e1f03a33e0e1f293be975c1a75
Esiakot <tony.parispro@gmail.com>
2026-01-05 09:11:14 +0100

    Correction: reactCompressor remplacé par reactCompiler dans la configuration Next.js

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompressor: true,
+  reactCompiler: true,
   images: {
     remotePatterns: [
       {
